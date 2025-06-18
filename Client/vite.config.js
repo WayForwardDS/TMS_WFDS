e199fad4e81042ac9e2ec172f0ac636395ef3964
Way Forward Digital Solutions <wayforwarddigitalsolutions@gmail.com>
2025-06-18 16:39:34 +0500

    Update deploy vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+//   darkMode: "class",
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public', // <---- This ensures Vite copies public files like _redirects
   darkMode: "class",
 })
